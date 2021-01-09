6dbf9fb8aa1d53c9652fa33ac3d3118db5e2a726
Jake Thrasher <mjakethrasher@gmail.com>
2021-01-08 16:58:07 -0800

    start madlib framework

---
+++ app.js
@@ -0,0 +1,17 @@
+// import functions and grab DOM elements
+const nounOne = document.getElementById('noun-1');
+const verbOne = document.getElementById('verb-1');
+const verbTwo = document.getElementById('verb-2');
+const verbThree = document.getElementById('verb-3');
+const submitButton = document.getElementById('submit-button');
+
+const inputOne= document.getElementById('input-one');
+const inputTwo= document.getElementById('input-two');
+const inputThree= document.getElementById('input-three');
+const inputFour= document.getElementById('input-four');
+// initialize state
+
+// set event listeners to update state and DOM
+submitButton.addEventListener('click', ()=>{
+    
+})
